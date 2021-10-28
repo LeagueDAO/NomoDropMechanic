264a677c0b95b10adc3185244d86ef41e2a56ba7
LuboslavBurdenski <luboslav990226@gmail.com>
2021-10-28 11:21:09 +0300

    add Solidity doc in `Attacker` contract

--- contracts/mocks/Attacker.sol
+++ contracts/mocks/Attacker.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.3;
 
 import "../NomoPlayersDropMechanic.sol";
 
+/**
+ * @title Contract for testing vulnerability, which could be an object of attack.
+ */
 contract Attacker {
     constructor(address nomoPlayersDropMechanicAddr)  {
         NomoPlayersDropMechanic(nomoPlayersDropMechanicAddr).buyTokensOnSale(1);
