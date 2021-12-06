c9f3f88ecdd9c179ffe0b396f26b9f30f773c597
LuboslavBurdenski <luboslav990226@gmail.com>
2021-12-06 14:43:02 +0200

    change number of tokens to be added to `NomoDropMechanic`

--- test/helpers/constants.ts
+++ test/helpers/constants.ts
@@ -2,7 +2,7 @@ export const tokenPrice: string = (1 * 10 ** 18).toString();
 
 export const zeroAddress = "0x0000000000000000000000000000000000000000";
 
-export const collectibleItems: number = 351;
+export const collectibleItems: number = 1962;
 
 export const maxQuantity: number = 3;
 
